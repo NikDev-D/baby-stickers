8ad23da136057ed5ac30263564116a16e94df73a
NikDev-D <moi.glaza.vezde@gmail.com>
2026-05-25 10:14:28 +0500

    feat: add editor widget and connect to app index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
+import { EditorScreen } from "@/widgets/editor";
 
 export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>BabyStickers 🌟</Text>
-    </View>
-  );
+  return <EditorScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
